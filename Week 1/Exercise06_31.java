6948be20a319b561d0d52e0e5b7d862674c30e53
Sam Ragsdale <lunesazul@duck.com>
2025-06-17 22:18:20 -0400

    updates, week 2

--- Week 1/Exercise06_31.java
+++ Week 1/Exercise06_31.java
@@ -9,21 +9,44 @@ public class Exercise06_31 {
         return true;
     }
 
-    //Iterate the long number to process per digit as an int
-    public void processLongAsInt(long number) {
+    /** Iterate the long number to process per digit as an int */
+    public static long sumOfOddPlace(long number) {
+        boolean shouldAdd = true;
+        int sum = 0;
+        while(number != 0) {
+            int digit = (int)(number % 10);
+            if(shouldAdd) { 
+            sum += digit;        
+           
+            System.out.println("The odd sum is: " + sum + "\n");
+            
+        }
+        shouldAdd = !shouldAdd;
+        number /= 10;
+    }
+    return (int)(sum);    
+}
+
+    public static long sumOfDoubleEvenPlace(long number) {
         boolean shouldAdd = false;
         int sum = 0;
-        if(shouldAdd) {
-            while (number != 0) { 
+        while(number != 0) {
             int digit = (int)(number % 10);
-            sum += digit;
+            if(shouldAdd) {
+            int digitSum = digit * 2;
+                if(digitSum >= 10) {
+                    int temp = (digitSum / 10) + (digitSum % 10);
+                    System.out.println("Sum of temp is: " + temp);
+                    sum += temp; 
+                }     
+            sum += digitSum;        
+            System.out.println("The even sum is: " + sum + "\n");
+            
+            }
             shouldAdd = !shouldAdd;
             number /= 10;
-            
-        
-        getDigit(digit);
-            }
-        }         
+        }
+        return (int)(sum);   
     }
         
 
@@ -33,26 +56,12 @@ public class Exercise06_31 {
         int sum = (result < 10) ? result : (result / 10) + (result % 10);
         System.out.println("Sum is: " + sum);
         System.out.println("Result is: " + result);
+        return result;
         }
         
-        
+
+
     
-
-    /** Get the result from Step 2 */
-    // public static int sumOfDoubleEvenPlace(long number) {
-    //     long result = 0;
-    //     int tmp = 0;
-    //     while(number > 0) {
-    //         tmp = number % 10;
-    //         result += tmp * tmp;
-    //         number /= 10;
-    //     }
-    //     return result;
-    // }
-
-    /** Return this number if it is a single digit, otherwise
-     * return the sum of the two digits
-     */
 
 
 
@@ -61,73 +70,19 @@ public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         System.out.println("Enter a credit card number between 13 and 16 digits: ");
-        long number = input.nextLong();
+        long userNumber = input.nextLong();
 
-        
-        // boolean shouldAdd = false;
-        // int sum = 0;
-        // while (number > 0) {
-        //     int digit = (int)(number % 10);
-        //     if(shouldAdd) {
-        //         if(digit * 2 >= 10) {
-        //             digit *= 2;
-        //             sum += digit % 10;
-                
-                    
-        //         // sum += ((digit / 10) + (digit % 10));
-        //         System.out.println("Sum in digit >= 10 is: " + sum);   
-        //         System.out.println("Digit >= 10 is: " + digit);
-                
-        //         }
-        //         else if(digit * 2 < 10);
-        //             sum += digit * 2;
-        //     }
-        //     shouldAdd = !shouldAdd;
-        //     number /= 10;
-        //     System.out.println("Digit is " + digit);
-        //     System.out.println("Sum is: " + sum);
-        // }
-    
+        // System.out.println("The sum of " + userNumber + " is " + sumOfOddPlace(userNumber));
 
+        long oddDigits = sumOfOddPlace(userNumber);
+        long evenDigits = sumOfDoubleEvenPlace(userNumber);
+
+        System.out.println("the value of evenDigits is: " + evenDigits);
+        System.out.println("the value of oddDigits is: " + oddDigits);
     }
-
-    // /** Return sum of odd-place digits in number */
-    // public static int sumOfOddPlace(long number) {
-    //     return number;
-    // }
-    
-    // /** Return the number of digits in d */
-    // public static int getSize(long d) {
-    //     return d;
-    // }
-    
-    // /** Return the first k number of digits from the number.
-    //  * If the number of digits is less than k, return number. */
-    // public static long getPrefix(long number, int k) {
-    //     return number;
-    // }
-
-    
-        
+}
 
 
-
-        // int sum=0;
-        // boolean shouldAdd = true;
-
-        // while (number > 0) {
-        //     int digit = (int)(number % 10);
-        //     if (shouldAdd) {
-        //         sum += digit;
-        //     // System.out.println(sum);
-        //     System.out.println(digit);
-        //     }
-        //     shouldAdd = !shouldAdd;
-        //     number /= 10;
-        // }
-        
-
-    
 
 
 
--- Week 1/Exercise08_29.java
+++ Week 1/Exercise08_29.java
@@ -1,5 +1,11 @@
 import java.util.Scanner;
 
 public class Exercise08_29 {
-    
+    public static void main(String[] args) {
+        //Init scanner
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter m1 (a 3 by 3 matrix) row by row: ");
+        int[][] m1 = input.
+    }    
 }
