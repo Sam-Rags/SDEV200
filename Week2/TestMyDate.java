01f1eed10ef7efa20aa8f9b36d6a265b0fbec025
Sam Ragsdale <lunesazul@duck.com>
2025-06-19 13:48:05 -0400

    Project submission

--- Week2/TestMyDate.java
+++ Week2/TestMyDate.java
@@ -1,23 +1,23 @@
-import Week2.MyDate;
+package Week2;
 
 public class TestMyDate {
 	
 	public static void main(String[] args) {
 		//no-arg contructor
 		MyDate myDate = new MyDate();
-		System.out.println("Current Date:"+myDate.getDay()+"/"+myDate.getMonth()+"/"+myDate.getYear());
+		System.out.println("Current Date: " + myDate.getMonth() + "/" + myDate.getDay() + "/" + myDate.getYear());
 		
 		//A constructor with a specified year, month and day
 		MyDate myDate1 = new MyDate(12, 5, 2020);
-		System.out.println("Date:"+myDate1.getDay()+"/"+myDate1.getMonth()+"/"+myDate1.getYear());
+		System.out.println("Date: " + myDate1.getMonth() + "/" + myDate1.getDay() + "/" + myDate1.getYear());
 		
 		//elapsed time
-		MyDate myDate2 = new MyDate(0L);
-		System.out.println("Date:"+myDate2.getDay()+"/"+myDate2.getMonth()+"/"+myDate2.getYear());
+		MyDate myDate2 = new MyDate(34355555133101L);
+		System.out.println("elapse time is Date: " + myDate2.getMonth() + "/" + myDate2.getDay() + "/" + myDate2.getYear());
 		
 		//setTime
-		myDate2.setDate(1639998855800L);
-		System.out.println("Date:"+myDate2.getDay()+"/"+myDate2.getMonth()+"/"+myDate2.getYear());
+		myDate2.setDate(0L);
+		System.out.println("myDate set at Date: " + myDate2.getDay() + "/"+myDate2.getMonth() + "/"+myDate2.getYear());
 		
 	}
 }
